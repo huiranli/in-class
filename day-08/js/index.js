24eb666ecca87764de2e5c37fd933a2f41291d77
rosey412 <huiranli@uw.edu>
2020-04-29 16:39:17 -0700

    add demo changes

--- day-08/js/index.js
+++ day-08/js/index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log("hello world");
+
+//takes in TWO callback functions!
+function doTogether(firstCallback, secondCallback){
+    firstCallback();  //execute the first function
+    secondCallback();  //execute the second function
+    console.log('at the same time!');
+}
+
+function patHead() {
+    console.log('pat your head');
+}
+
+function rubBelly() {
+    console.log('rub your belly');
+}
+
+//pass in the callbacks to do them together
+doTogether(patHead, rubBelly);
